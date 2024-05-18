6c5e040c2b2962d1d14059427983b61a81da949b
cjrace <52536248+cjrace@users.noreply.github.com>
2024-05-18 12:30:47 +0100

    add comments

--- app.R
+++ app.R
@@ -2,27 +2,21 @@ library(shiny)
 library(shiny.router)
 
 main_page <- function() {
-  div(
-    h1("Main page"),
-    p("Some main text.")
-  )
+  div(h1("Main page"), p("Some main text."))
 }
 
 sub_page <- function() {
-  div(
-    h1("Sub page"),
-    p("Some subtext.")
-  )
+  div(h1("Sub page"), p("Some subtext."))
 }
 
 ui <- fluidPage(
   tags$ul(
     tags$li(tags$a("Main page", href = "/")),
-    tags$li(tags$a("Sub page", href = "/#!/subpage"))
+    tags$li(tags$a("Sub page", href = "/#!/subpage")) # This works locally, but takes me to https://cjrace.shinyapps.io/#!/subpage when deployed
   ),
   router_ui(
-    route("/", main_page()), # want this at
-    route("subpage", sub_page()) # want this at
+    route("/", main_page()), # This appears after a slight delay and redirecting to https://cjrace.shinyapps.io/shiny-routing-reprex/_w_9c312d7b/#!/
+    route("subpage", sub_page()) # This is showing at https://cjrace.shinyapps.io/shiny-routing-reprex/_w_ac80e61b/#!/subpage
   )
 )
 
